33c45e22a2080c03c9561a931c3300e38628c62e
SinaBoby <sinaboby91@yahoo.com>
2022-06-01 11:33:18 +0200

    schemas added

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,24 +2,41 @@ import mongoose from "mongoose";
 
 import validateAllowedFields from "../util/validateAllowedFields.js";
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true, trim: true },
-  userName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true },
-  password: { type: String, required: true },
-  userType: {
-    type: String,
-    enum: ["NewComer", "Local"],
-    required: true,
-    default: "NewComer",
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    userName: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true },
+    userType: {
+      type: String,
+      enum: ["NewComer", "Local"],
+      required: true,
+      default: "NewComer",
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      alt: String,
+      img: {
+        data: Buffer,
+        contentType: String,
+      },
+    },
+    birthDay: { type: Date, required: true },
+    joinedAt: { type: Date, default: () => Date.now(), immutable: true },
+    interests: [String],
+    isActive: Boolean,
+    createdActivities: [
+      { type: mongoose.SchemaTypes.ObjectId, ref: "Activity" },
+    ],
+    activities: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Activity" }],
   },
-  phoneNumber: String,
-  birthDay: { type: Date, required: true },
-  joinedAt: { type: Date, default: () => Date.now(), immutable: true },
-  interests: [String],
-  isActive: Boolean,
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("users", userSchema);
 
@@ -37,6 +54,9 @@ export const validateUser = (userObject) => {
     "joinedAt",
     "interests",
     "isActive",
+    "profileImage",
+    "createdActivities",
+    "activities",
   ];
 
   const validatedKeysMessage = validateAllowedFields(userObject, allowedKeys);
@@ -45,32 +65,28 @@ export const validateUser = (userObject) => {
     errorList.push(validatedKeysMessage);
   }
 
-  if (userObject.firstName == null) {
+  if (userObject.firstName === null) {
     errorList.push("firstName is a required field");
   }
 
-  if (userObject.lastName == null) {
+  if (userObject.lastName === null) {
     errorList.push("lastName is a required field");
   }
 
-  if (userObject.userName == null) {
+  if (userObject.userName === null) {
     errorList.push("userName is a required field");
   }
 
-  if (userObject.email == null) {
+  if (userObject.email === null) {
     errorList.push("email is a required field");
   }
 
-  if (userObject.password == null) {
+  if (userObject.password === null) {
     errorList.push("password is a required field");
   }
 
-  if (userObject.birthDay == null) {
+  if (userObject.birthDay === null) {
     errorList.push("birthDay is a required field");
-  }
-
-  if (userObject.interests == null) {
-    errorList.push("interests is a required field");
   }
 
   return errorList;
