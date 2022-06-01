5daafe713523ad2dad4cddf2d9650e35eff4dc29
Burak Özmen <brkzmn.wd@gmail.com>
2022-06-01 21:38:10 +0200

    css added

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import "./SignUp.css";
 import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 const SignUp = () => {
   return (
     <div className="signup-container">
-      <form>
+      <form onSubmit={() => {}}>
         <h2>Sign Up</h2>
         <div className="signup-input-field">
           <label>First Name</label>
-          <Input name="firstName" value={name} />
+          <Input name="firstName" value={name} required />
         </div>
         <div className="signup-input-field">
           <label>Last Name</label>
-          <Input name="lastName" value={name} />
+          <Input name="lastName" value={name} required />
         </div>
         <div className="signup-input-field">
           <label>Username</label>
-          <Input name="userName" value={name} />
+          <Input name="userName" value={name} required />
         </div>
         <div className="signup-input-field">
           <label>Email</label>
-          <Input name="email" value={name} />
+          <Input name="email" value={name} required />
         </div>
         <div className="signup-input-field">
           <label>Password</label>
-          <Input name="password" value={name} />
+          <Input name="password" type="password" value={name} required />
         </div>
         <div className="signup-input-field">
           <label>User Type</label>
-          <Input name="userType" value={name} />
+          <Input name="userType" value={name} required />
         </div>
         <div className="signup-input-field">
           <label>Phone Number</label>
@@ -37,12 +38,13 @@ const SignUp = () => {
         </div>
         <div className="signup-input-field">
           <label>Birth Day</label>
-          <Input name="birthDay" value={name} />
+          <Input name="birthDay" value={name} required />
         </div>
         <div className="signup-input-field">
           <label>Interests</label>
           <Input name="Interests" value={name} />
         </div>
+        <Button className="btn-link navbar-link">Sign Up</Button>
       </form>
     </div>
   );
