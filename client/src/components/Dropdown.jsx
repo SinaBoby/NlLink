7f58b3256d1babd8e388be17d8da23c092cab812
bashar khdr <bashar.khdr.DS.92@hotmail.com>
2022-06-11 15:17:23 +0200

    refactoring dropdown mobile menu

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
 import "./Dropdown.css";
 import { useNavigate } from "react-router-dom";
 import Divider from "./Divider";
 import DropdownLink from "./DropdownLink";
+import { AuthContext } from "../AuthContext";
+import useUserDetails from "../hooks/useUserDetails";
 
-const Dropdown = () => {
+const Dropdown = ({ closeDropdown }) => {
   const navigate = useNavigate();
+
+  const { isAuthenticated } = useContext(AuthContext);
+  const { userDetails } = useUserDetails();
+
+  const handleNavigation = (route) => {
+    closeDropdown();
+    navigate(route);
+  };
 
   return (
     <div className="dropdown">
       <DropdownLink>News</DropdownLink>
-      <DropdownLink onClick={() => navigate("/about")}>About</DropdownLink>
-      <DropdownLink onClick={() => navigate("/contact")}>
+      <DropdownLink onClick={() => handleNavigation("/about")}>
+        About
+      </DropdownLink>
+      <DropdownLink onClick={() => handleNavigation("/contact")}>
         Contact us
       </DropdownLink>
+      {isAuthenticated && userDetails && (
+        <DropdownLink onClick={() => handleNavigation("/connect")}>
+          {userDetails.userType === "NewComer"
+            ? "Connect to Locals"
+            : "Connect to New Comers"}
+        </DropdownLink>
+      )}
+      {isAuthenticated && (
+        <DropdownLink onClick={() => handleNavigation("/activities")}>
+          Activities
+        </DropdownLink>
+      )}
       <Divider />
-      <DropdownLink onClick={() => navigate("/login")}>Login</DropdownLink>
-      <DropdownLink onClick={() => navigate("/user/create")}>
-        Register
-      </DropdownLink>
+      {!isAuthenticated ? (
+        <>
+          <DropdownLink onClick={() => handleNavigation("/login")}>
+            Login
+          </DropdownLink>
+          <DropdownLink onClick={() => handleNavigation("/user/create")}>
+            Register
+          </DropdownLink>
+        </>
+      ) : (
+        <>
+          <DropdownLink onClick={() => handleNavigation("/dashboard")}>
+            Profile
+          </DropdownLink>
+          <DropdownLink onClick={() => handleNavigation("/logout")}>
+            Logout
+          </DropdownLink>
+        </>
+      )}
     </div>
   );
+};
+
+Dropdown.propTypes = {
+  closeDropdown: PropTypes.func.isRequired,
 };
 
 export default Dropdown;
