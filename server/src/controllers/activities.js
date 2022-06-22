28f8eb5e757bbc715a14ab88e58a211b58c249c2
Burak Özmen <brkzmn.wd@gmail.com>
2022-06-22 10:55:54 +0200

    activity route modified

--- server/src/controllers/activities.js
+++ server/src/controllers/activities.js
@@ -2,6 +2,7 @@ import Activity from "../models/Activity.js";
 import User from "../models/User.js";
 import mongoose from "mongoose";
 // import { logInfo } from "../util/logging.js";
+import { logError, logInfo } from "../util/logging.js";
 
 export const getUserActivities = async (req, res) => {
   try {
@@ -35,10 +36,79 @@ export const getActivities = async (req, res) => {
         success: true,
         result: allActivities,
       });
+    } else {
+      const selectedActivities = await Activity.find({
+        category: { $in: [activityCategory] },
+      });
+      res.status(200).json({
+        success: true,
+        result: selectedActivities,
+      });
     }
   } catch (error) {
-    {
-      res.send({ msg: "Error in Fetching Selected Activities" });
+    const errors = [];
+    if (error.errors) {
+      Object.keys(error.errors).forEach((key) => {
+        errors.push(`${key} : ${error.errors[key].message}`);
+      });
+      logError(error);
+      return res
+        .status(400)
+        .json({ success: false, msg: `Bad Request: ${errors}` });
+    } else {
+      logError(error);
+      return res.status(500).json({
+        success: false,
+        msg: `Server Error: ${error.message}`,
+      });
+    }
+  }
+};
+
+export const createActivity = async (req, res) => {
+  try {
+    const activityId = req.activityId;
+    logInfo(activityId);
+  } catch (error) {
+    const errors = [];
+    if (error.errors) {
+      Object.keys(error.errors).forEach((key) => {
+        errors.push(`${key} : ${error.errors[key].message}`);
+      });
+      logError(error);
+      return res
+        .status(400)
+        .json({ success: false, msg: `Bad Request: ${errors}` });
+    } else {
+      logError(error);
+      return res.status(500).json({
+        success: false,
+        msg: `Server Error: ${error.message}`,
+      });
+    }
+  }
+};
+
+export const deleteActivity = async (req, res) => {
+  try {
+    const userName = req.userName;
+    logInfo(userName);
+  } catch (error) {
+    const errors = [];
+    if (error.errors) {
+      Object.keys(error.errors).forEach((key) => {
+        errors.push(`${key} : ${error.errors[key].message}`);
+      });
+      logError(error);
+      return res
+        .status(400)
+        .json({ success: false, msg: `Bad Request: ${errors}` });
+    } else {
+      logError(error);
+      return res.status(500).json({
+        success: false,
+        msg: `Server Error: ${error.message}`,
+      });
     }
   }
 };
--- server/src/routes/activities.js
+++ server/src/routes/activities.js
@@ -1,14 +1,17 @@
 import express from "express";
 import withAuth from "../middlewares/middleware.js";
-import { getUserActivities } from "../controllers/activities.js";
+import {
+  getUserActivities,
+  getActivities,
+  createActivity,
+  deleteActivity,
+} from "../controllers/activities.js";
 
 const activitiesRouter = express.Router();
 
 activitiesRouter.get("/user-activities", withAuth, getUserActivities);
-activitiesRouter.get(
-  "/category/:activityCategory",
-  withAuth,
-  getUserActivities
-);
+activitiesRouter.get("/category/:activityCategory", withAuth, getActivities);
+activitiesRouter.post("/create", withAuth, createActivity);
+activitiesRouter.delete("/delete", withAuth, deleteActivity);
 
 export default activitiesRouter;
