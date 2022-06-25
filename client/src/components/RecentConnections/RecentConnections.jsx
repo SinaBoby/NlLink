090a52cb176057f5a955f61aa32a7a6ad0a80495
Sina Boby <sinaboby91@yahoo.com>
2022-06-26 00:36:23 +0200

    socket event to get contacts

--- client/src/components/RecentConnections/RecentConnections.jsx
+++ client/src/components/RecentConnections/RecentConnections.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { SocketContext } from "../../SocketContext";
-import useUserDetails from "../../hooks/useUserDetails";
-import { logInfo } from "../../../../server/src/util/logging.js";
-import useFetch from "../../hooks/useFetch";
-import Spinner from "../../components/Spinner/Spinner";
-import Error from "../../components/Error/Error";
+//import useUserDetails from "../../hooks/useUserDetails";
+//import { logInfo } from "../../../../server/src/util/logging.js";
+//import useFetch from "../../hooks/useFetch";
+//import Spinner from "../../components/Spinner/Spinner";
+//import Error from "../../components/Error/Error";
 import "./RecentConnections.css";
 import UserCard from "./UserCard";
 
@@ -13,33 +13,10 @@ import UserCard from "./UserCard";
 const RecentConnections = ({ userId }) => {
   const [contacts, setContacts] = useState([]);
   const [contactsIds, setContactsIds] = useState([]);
-  const { userDetails, isMeLoading, meError, cancelMeFetch } = useUserDetails();
+  //const { userDetails, isMeLoading, meError, cancelMeFetch } = useUserDetails();
   const [messagesFull, setMessagesFull] = useState([]);
   const navigate = useNavigate();
-  logInfo(contactsIds);
-  useEffect(() => {
-    const idArray = [];
-    messagesFull.forEach((msg) => {
-      if (msg.sender === userId) {
-        if (!idArray.includes(msg.receiver)) {
-          idArray.push(msg.receiver);
-        }
-      } else if (msg.receiver === userId) {
-        if (!idArray.includes(msg.sender)) {
-          idArray.push(msg.sender);
-        }
-      }
-    });
-    idArray.forEach((id) => {
-      if (id !== userId) {
-        logInfo(id);
-        if (contactsIds.indexOf(id) === -1) {
-          logInfo(id);
-          setContactsIds((prevIds) => [...prevIds, id]);
-        }
-      }
-    });
-  }, [messagesFull]);
+  // logInfo(contactsIds);
   const { socket } = useContext(SocketContext);
   const addMessage = (msg) => {
     setMessagesFull((oldMessages) => [
@@ -51,30 +28,52 @@ const RecentConnections = ({ userId }) => {
     socket.connect();
 
     socket.on("chatHistory", (data) => {
-      logInfo(data);
+      //logInfo(data);
       data.forEach((chat) => {
         const idArray = chat._id.split(" ").filter((elem) => elem !== "and");
-        logInfo(idArray);
+        //logInfo(idArray);
         if (idArray.includes(userId)) {
           addMessage(chat.messages);
         }
       });
     });
+    socket.on("sendContacts", (contacts) => setContacts(contacts));
     return () => {
       socket.disconnect();
-      cancelMeFetch();
-      cancelFetch();
+      //cancelMeFetch();
+      //cancelFetch();
     };
   }, []);
-  const onSuccess = (response) => {
+  useEffect(() => {
+    const idArray = [];
+    messagesFull.forEach((msg) => {
+      if (msg.sender === userId && !idArray.includes(msg.receiver)) {
+        idArray.push(msg.receiver);
+      } else if (msg.receiver === userId && !idArray.includes(msg.sender)) {
+        idArray.push(msg.sender);
+      }
+    });
+    idArray &&
+      idArray.forEach((id) => {
+        if (id !== userId) {
+          //logInfo(id);
+          if (!contactsIds.includes(id)) {
+            //logInfo(id);
+            setContactsIds((prevIds) => [...prevIds, id]);
+          }
+        }
+      });
+  }, [messagesFull]);
+
+  /*  const onSuccess = (response) => {
     setContacts(response.contacts);
   };
   const { isLoading, error, performFetch, cancelFetch } = useFetch(
     "/contacts",
     onSuccess
-  );
+  ); */
   useEffect(() => {
-    contactsIds &&
+    /* contactsIds &&
       performFetch({
         method: "POST",
         headers: {
@@ -82,17 +81,17 @@ const RecentConnections = ({ userId }) => {
         },
         credentials: "include",
         body: JSON.stringify({ contactsIds }),
-      });
-
+      }); */
+    contactsIds && socket.emit("contacts", contactsIds);
     //return cancelFetch;
   }, [contactsIds]);
 
   return (
     <div className="recent-connections">
-      {isMeLoading && !meError && <Spinner />}
-      {meError && <Error>{meError}</Error>}
-      {isLoading && !error && <Spinner />}
-      {error && <Error>{error}</Error>}
+      {/* {isMeLoading && !meError && <Spinner />}
+      {meError && <Error>{meError}</Error>} */}
+      {/* {isLoading && !error && <Spinner />}
+      {error && <Error>{error}</Error>} */}
       <h2 className="recent-connections-title">Connections</h2>
       <div className="recent-connections-list">
         <>
@@ -107,7 +106,7 @@ const RecentConnections = ({ userId }) => {
                       //localStorage.setItem("receiver", JSON.stringify(user));
                       //socket.emit("refresh", "messagesList")
                       navigate(`/chat/${user._id}`, {
-                        state: { receiver: user, userId: userDetails._id },
+                        state: { receiver: user, userId: userId },
                       });
                     }}
                   />
